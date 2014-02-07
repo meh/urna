f2c83db836d1336d8af73447a5f8545f0dd33456
meh <meh@schizofreni.co>
2014-02-07 14:21:41 +0100

    mix: update to latest elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Urna.Mixfile do
   def project do
     [ app: :urna,
       version: "0.0.1",
-      elixir: "~> 0.11.0 or ~> 0.12.0",
+      elixir: "~> 0.12.3",
       deps: deps ]
   end
 

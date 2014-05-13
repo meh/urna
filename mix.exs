94cae3d964037f69924923259e0f58e765a39dd0
meh <meh@schizofreni.co>
2014-05-13 23:39:55 +0200

    mix: update definitions and requirements

--- mix.exs
+++ mix.exs
@@ -3,13 +3,25 @@ defmodule Urna.Mixfile do
 
   def project do
     [ app: :urna,
-      version: "0.0.1",
-      elixir: "~> 0.13.2-dev",
-      deps: deps ]
+      version: "0.1.0",
+      elixir: "~> 0.13.2",
+      deps: deps,
+      package: package,
+      description: "REST in peace" ]
+  end
+
+  defp package do
+    [ contributors: ["meh"],
+      license: ["WTFPL"],
+      links: [ { "GitHub", "https://github.com/meh/urna" } ] ]
+  end
+
+  def application do
+    [ applications: [:cauldron, :jazz] ]
   end
 
   defp deps do
-    [ { :cauldron, github: "meh/cauldron" },
+    [ { :cauldron, "~> 0.1.0" },
       { :jazz, "~> 0.1.0" } ]
   end
 end

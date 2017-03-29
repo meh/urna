ebac832a2df0ed2d440e358b6cdfce09fded26a5
meh <meh@schizofreni.co>
2017-03-29 19:36:23 +0200

    mix: bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Urna.Mixfile do
 
   def project do
     [ app: :urna,
-      version: "0.1.5",
+      version: "0.2.0",
       deps: deps(),
       package: package(),
       description: "REST in peace" ]
